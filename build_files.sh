aab6662973ed2e39957db53d33da92de97cedca0
Gowtham Kumar Vulluri <gowthamkumarvulluri@gmail.com>
2023-06-06 16:03:39 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 echo "BUILD START"
+sudo apt-get install libsqlite3-dev
 python3.9 -m pip install django==3.2.10
+python3.9 -m pip install pysqlite
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
 echo "BUILD END"
