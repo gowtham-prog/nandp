35199ed04c81a1c98b8d95463574034b24554654
Gowtham Kumar Vulluri <gowthamkumarvulluri@gmail.com>
2023-06-06 20:35:58 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
 python3.9 -m pip install django==3.2.10
+python3.9 -m pip install psycopg2
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
 echo "BUILD END"
