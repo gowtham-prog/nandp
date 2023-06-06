f68ac4e1e7941616e47585e66dd816d05701871e
Gowtham Kumar Vulluri <gowthamkumarvulluri@gmail.com>
2023-06-06 15:57:15 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.9 -m pip install virtualenv
-venv\Scripts\activate
+python3.9 -m pip install django==3.2.10
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
 echo "BUILD END"
