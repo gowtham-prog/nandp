d90b29c523d596b1dc48f60e97b4e7cc55f2beb1
Gowtham Kumar Vulluri <gowthamkumarvulluri@gmail.com>
2023-06-06 15:54:59 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD START"
-python3.9 -m venv\Scripts\activate
+python3.9 -m pip install virtualenv
+venv\Scripts\activate
 python3.9 manage.py collectstatic --no-input --clear
 echo "BUILD END"
